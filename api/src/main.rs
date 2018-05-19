8f8511ed4f54b83dacdf3235df9e35498a2b4bfc
Yosef da Iry <yosefda@gmail.com>
2018-05-19 23:23:17 +1000

    created app module

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,9 @@
 extern crate actix_web;
 
-use actix_web::{server, App, HttpRequest};
+mod app;
 
-fn index(_req: HttpRequest) -> &'static str {
-    "Hello world!"
-}
+use app::App;
 
 fn main() {
-    server::new(
-        || App::new()
-            .resource("/", |r| r.f(index)))
-        .bind("0.0.0.0:3000").expect("Can not bind to 0.0.0.0:3000")
-        .run();
+    App::run();
 }
