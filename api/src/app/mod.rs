d6922f81617e876c9db00b4bc9574b07b85eb240
Yosef da Iry <yosefda@gmail.com>
2018-05-20 00:06:47 +1000

    created controller

---
+++ api/src/app/mod.rs
@@ -0,0 +1,17 @@
+mod controller;
+
+use actix_web::server;
+use actix_web::App as ActixWebApp;
+
+use self::controller::Controller;
+
+pub struct App {}
+
+impl App {
+    pub fn run() {
+        server::new(|| ActixWebApp::new().resource("/", |r| r.h(Controller {})))
+            .bind("0.0.0.0:3000")
+            .expect("Can not bind to 0.0.0.0:3000")
+            .run();
+    }
+}
