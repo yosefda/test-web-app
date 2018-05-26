8539950dc8155b736f2bbb498ff3dc0605a45405
Yosef da Iry <yosefda@gmail.com>
2018-05-27 01:51:02 +1000

    simple routing

--- api/src/service.rs
+++ api/src/service.rs
@@ -3,10 +3,9 @@ use futures::future::ok as future_ok;
 use hyper::header::ContentLength;
 use hyper::server::{Request, Response, Service};
 use hyper::Error as HyperError;
+use hyper::{Method, StatusCode};
 
 pub struct WebAppApi;
-
-const PHRASE: &'static str = "Hello world!";
 
 impl Service for WebAppApi {
     // hyper's server types
@@ -15,11 +14,19 @@ impl Service for WebAppApi {
     type Error = HyperError;
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
-        Box::new(future_ok(
-            Response::new()
-                .with_header(ContentLength(PHRASE.len() as u64))
-                .with_body(PHRASE)
-        ))
+    fn call(&self, req: Request) -> Self::Future {
+        let mut response = Response::new();
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                response.set_body("Test Web App Api");
+            },
+            (&Method::Post, "/echo") => {}
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            },
+        };
+
+        Box::new(future_ok(response))
     }
 }
